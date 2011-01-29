8955fa407a7e48c514c4a11944c939accb6715fc
Henning Schmiedehausen <henning@ning.com>
2011-01-29 11:22:38 -0800

    Remove various new RuntimeException() with a JmxException that can be asked for the reason of the exception to determine action depending on the cause.

---
+++ src/main/java/org/weakref/jmx/JmxException.java
@@ -0,0 +1,41 @@
+package org.weakref.jmx;
+
+public class JmxException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public enum JmxCause
+    {
+        CONFIG,
+        MALFORMED_OBJECT_NAME,
+        INSTANCE_ALREADY_EXISTS,
+        INSTANCE_NOT_FOUND,
+        MBEAN_REGISTRATION,
+        NOT_COMPLIANT_MBEAN;
+    }
+
+    private final JmxCause jmxCause;
+
+    JmxException(final JmxCause jmxCause, final String message, final Object ... args)
+    {
+        super(String.format(message, args));
+        this.jmxCause = jmxCause;
+    }
+
+    JmxException(final JmxCause jmxCause, final Throwable throwable)
+    {
+        super(throwable);
+        this.jmxCause = jmxCause;
+    }
+
+    JmxException(final JmxCause jmxCause, final Throwable cause, final String message, final Object ... args)
+    {
+        super(String.format(message, args), cause);
+        this.jmxCause = jmxCause;
+    }
+
+    public JmxCause getJmxCause()
+    {
+        return jmxCause;
+    }
+}
