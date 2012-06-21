7cd3b649ecd7440c5409129d554a542a85c2545e
Martin Traverso <martint@fb.com>
2012-06-21 14:57:35 -0700

    delete InternalMBeanModule
    No longer needed in Guice 3 due to support for identical duplicate bindings.

--- src/main/java/org/weakref/jmx/guice/MBeanModule.java
+++ src/main/java/org/weakref/jmx/guice/MBeanModule.java
@@ -31,7 +31,7 @@ public class MBeanModule
     {
         builder = newExporter(binder());
 
-        install(new InternalMBeanModule());
+        bind(GuiceMBeanExporter.class).asEagerSingleton();
         configureMBeans();
     }
 
