8206093933daf71eb4c238af82512e17e9208ed3
arhimondr <andriyrosa@gmail.com>
2015-08-30 23:25:25 +0200

    add cacheDurationMillis attribute to Flatten and Nested annotations

--- src/main/java/org/weakref/jmx/MBeanAttributeBuilder.java
+++ src/main/java/org/weakref/jmx/MBeanAttributeBuilder.java
@@ -19,15 +19,18 @@ import static org.weakref.jmx.ReflectionUtils.isValidGetter;
 import static org.weakref.jmx.ReflectionUtils.isValidSetter;
 
 import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
 
 import javax.management.Descriptor;
 import javax.management.ImmutableDescriptor;
 import javax.management.MBeanAttributeInfo;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,14 +48,18 @@ public class MBeanAttributeBuilder
 
     public MBeanAttributeBuilder withTargetSupplier(Supplier targetSupplier)
     {
-        if (targetSupplier == null) throw new NullPointerException("targetSupplier is null");
+        if (targetSupplier == null) {
+            throw new NullPointerException("targetSupplier is null");
+        }
         this.targetSupplier = targetSupplier;
         return this;
     }
 
     public MBeanAttributeBuilder named(String name)
     {
-        if (name == null) throw new NullPointerException("name is null");
+        if (name == null) {
+            throw new NullPointerException("name is null");
+        }
         this.name = name;
         return this;
     }
@@ -135,13 +142,14 @@ public class MBeanAttributeBuilder
                 throw new IllegalArgumentException("Flattened JmxAttribute must have a concrete getter");
             }
 
-            Class targetType = getNestedTargetType(concreteGetter);
-            if(targetType == null){
+            Class nestedObjectType = getNestedObjectType(concreteGetter);
+            if (nestedObjectType == null) {
                 return ImmutableList.of();
             }
-            Supplier nestedObjectSupplier = createNestedObjectSupplier(targetType, concreteGetter);
+            long cacheDurationMillis = getNestedObjectCacheDuration(annotatedGetter);
+            Supplier nestedObjectSupplier = createNestedObjectSupplier(nestedObjectType, concreteGetter, cacheDurationMillis);
 
-            MBean mbean = MBeanBuilder.from(targetType, nestedObjectSupplier).build();
+            MBean mbean = MBeanBuilder.from(nestedObjectType, nestedObjectSupplier).build();
             ArrayList<MBeanFeature> features = new ArrayList<MBeanFeature>();
             features.addAll(mbean.getAttributes());
             features.addAll(mbean.getOperations());
@@ -153,13 +161,14 @@ public class MBeanAttributeBuilder
                 throw new IllegalArgumentException("Nested JmxAttribute must have a concrete getter");
             }
 
-            Class targetType = getNestedTargetType(concreteGetter);
-            if(targetType == null){
+            Class nestedObjectType = getNestedObjectType(concreteGetter);
+            if (nestedObjectType == null) {
                 return ImmutableList.of();
             }
-            Supplier nestedObjectSupplier = createNestedObjectSupplier(targetType, concreteGetter);
+            long cacheDurationMillis = getNestedObjectCacheDuration(annotatedGetter);
+            Supplier nestedObjectSupplier = createNestedObjectSupplier(nestedObjectType, concreteGetter, cacheDurationMillis);
 
-            MBean mbean = MBeanBuilder.from(targetType, nestedObjectSupplier).build();
+            MBean mbean = MBeanBuilder.from(nestedObjectType, nestedObjectSupplier).build();
             ArrayList<MBeanFeature> features = new ArrayList<MBeanFeature>();
             for (MBeanAttribute attribute : mbean.getAttributes()) {
                 features.add(new NestedMBeanAttribute(attributeName, attribute));
@@ -236,7 +245,7 @@ public class MBeanAttributeBuilder
         return null;
     }
 
-    private Class getNestedTargetType(Method concreteGetter)
+    private Class getNestedObjectType(Method concreteGetter)
     {
         try {
             Object value = concreteGetter.invoke(targetSupplier.get());
@@ -248,8 +257,24 @@ public class MBeanAttributeBuilder
         }
     }
 
-    private Supplier createNestedObjectSupplier(final Class requiredType, final Method concreteGetter){
-        return new Supplier() {
+    private long getNestedObjectCacheDuration(Method annotatedGetter)
+    {
+        long cacheDurationMillis = -1;
+        Nested nestedAnnotation = AnnotationUtils.findAnnotation(Nested.class, annotatedGetter);
+        if (nestedAnnotation != null) {
+            cacheDurationMillis = nestedAnnotation.cacheDurationMillis();
+        }
+        Flatten flattenAnnotation = AnnotationUtils.findAnnotation(Flatten.class, annotatedGetter);
+        if (flattenAnnotation != null) {
+            cacheDurationMillis = flattenAnnotation.cacheDurationMillis();
+        }
+        return cacheDurationMillis;
+    }
+
+    private Supplier createNestedObjectSupplier(final Class requiredType, final Method concreteGetter, long cacheDurationMills)
+    {
+        Supplier supplier = new Supplier()
+        {
             public Object get()
             {
                 try {
@@ -261,5 +286,11 @@ public class MBeanAttributeBuilder
                 }
             }
         };
+        if (cacheDurationMills >= 0) {
+            supplier = Suppliers.memoizeWithExpiration(supplier, cacheDurationMills, TimeUnit.MILLISECONDS);
+        } else {
+            supplier = Suppliers.memoize(supplier);
+        }
+        return supplier;
     }
 }
