586a7b871f50e27f4f6074c96ebfd19189abaa0c
Dain Sundstrom <dain@iq80.com>
2019-01-20 11:32:36 -0800

    Add missing override annotations

--- src/main/java/org/weakref/jmx/testing/TestingMBeanServer.java
+++ src/main/java/org/weakref/jmx/testing/TestingMBeanServer.java
@@ -29,6 +29,7 @@ public class TestingMBeanServer
 {
     private final Map<ObjectName, DynamicMBean> mbeans = new HashMap<>();
 
+    @Override
     public ObjectInstance registerMBean(Object object, ObjectName name)
             throws InstanceAlreadyExistsException
     {
@@ -52,6 +53,7 @@ public class TestingMBeanServer
         return new ObjectInstance(name, mbean.getMBeanInfo().getClassName());
     }
 
+    @Override
     public void unregisterMBean(ObjectName name)
             throws InstanceNotFoundException
     {
@@ -60,6 +62,7 @@ public class TestingMBeanServer
         }
     }
 
+    @Override
     public ObjectInstance getObjectInstance(ObjectName name)
             throws InstanceNotFoundException
     {
@@ -68,45 +71,53 @@ public class TestingMBeanServer
         return new ObjectInstance(name, mbean.getMBeanInfo().getClassName());
     }
 
+    @Override
     public Set<ObjectName> queryNames(ObjectName name, QueryExp query)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public Object getAttribute(ObjectName name, String attribute)
             throws InstanceNotFoundException, AttributeNotFoundException, ReflectionException, MBeanException
     {
         return getMBean(name).getAttribute(attribute);
     }
 
+    @Override
     public AttributeList getAttributes(ObjectName name, String[] attributes)
             throws InstanceNotFoundException
     {
         return getMBean(name).getAttributes(attributes);
     }
 
+    @Override
     public boolean isRegistered(ObjectName name)
     {
         return mbeans.containsKey(name);
     }
 
+    @Override
     public Integer getMBeanCount()
     {
         return mbeans.size();
     }
 
+    @Override
     public void setAttribute(ObjectName name, Attribute attribute)
             throws InstanceNotFoundException, InvalidAttributeValueException, AttributeNotFoundException, ReflectionException, MBeanException
     {
         getMBean(name).setAttribute(attribute);
     }
 
+    @Override
     public AttributeList setAttributes(ObjectName name, AttributeList attributes)
             throws InstanceNotFoundException
     {
         return getMBean(name).setAttributes(attributes);
     }
 
+    @Override
     public Object invoke(ObjectName name, String operationName, Object[] params, String[] signature)
             throws InstanceNotFoundException, ReflectionException, MBeanException
     {
@@ -115,6 +126,7 @@ public class TestingMBeanServer
         return mbean.invoke(operationName, params, signature);
     }
 
+    @Override
     public MBeanInfo getMBeanInfo(ObjectName name)
             throws InstanceNotFoundException
     {
@@ -137,126 +149,150 @@ public class TestingMBeanServer
     }
 
 
+    @Override
     public ObjectInstance createMBean(String className, ObjectName name)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ObjectInstance createMBean(String className, ObjectName name, ObjectName loaderName)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ObjectInstance createMBean(String className, ObjectName name, Object[] params, String[] signature)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ObjectInstance createMBean(String className, ObjectName name, ObjectName loaderName, Object[] params, String[] signature)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public Set<ObjectInstance> queryMBeans(ObjectName name, QueryExp query)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public String getDefaultDomain()
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public String[] getDomains()
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public void addNotificationListener(ObjectName name, NotificationListener listener, NotificationFilter filter, Object handback)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public void addNotificationListener(ObjectName name, ObjectName listener, NotificationFilter filter, Object handback)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public void removeNotificationListener(ObjectName name, ObjectName listener)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public void removeNotificationListener(ObjectName name, ObjectName listener, NotificationFilter filter, Object handback)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public void removeNotificationListener(ObjectName name, NotificationListener listener)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public void removeNotificationListener(ObjectName name, NotificationListener listener, NotificationFilter filter, Object handback)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public boolean isInstanceOf(ObjectName name, String className)
             throws InstanceNotFoundException
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public Object instantiate(String className)
             throws ReflectionException, MBeanException
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public Object instantiate(String className, ObjectName loaderName)
             throws ReflectionException, MBeanException, InstanceNotFoundException
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public Object instantiate(String className, Object[] params, String[] signature)
             throws ReflectionException, MBeanException
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public Object instantiate(String className, ObjectName loaderName, Object[] params, String[] signature)
             throws ReflectionException, MBeanException, InstanceNotFoundException
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ObjectInputStream deserialize(ObjectName name, byte[] data)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ObjectInputStream deserialize(String className, byte[] data)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ObjectInputStream deserialize(String className, ObjectName loaderName, byte[] data)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ClassLoader getClassLoaderFor(ObjectName mbeanName)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ClassLoader getClassLoader(ObjectName loaderName)
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public ClassLoaderRepository getClassLoaderRepository()
     {
         throw new UnsupportedOperationException();
